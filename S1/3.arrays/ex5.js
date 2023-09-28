41044039a7b81f25eb47a6bed07628110ffe22e9
Ricardo De Esteban Garau <destgar@gmail.com>
2023-09-28 18:37:06 +0200

    Ejercicios hasta s2 > conditionals

---
+++ S1/3.arrays/ex5.js
@@ -0,0 +1,7 @@
+//Elimina el último elemento del array y muestra el primero y el último por consola.
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters.pop()
+
+console.log(rickAndMortyCharacters[0])
---
+++ S1/5.loops/ex1.js
@@ -0,0 +1,8 @@
+//Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+var miBucle = [];
+
+for(i = 0 ; i <= 9 ; i++){
+    miBucle.push(i)
+}
+
+console.log(miBucle)
